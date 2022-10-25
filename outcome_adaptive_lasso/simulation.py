792831eaaf7c235c5d1b1b1991717cddf7de5c5c
Harsh Sinha <harsh.sinha@pitt.edu>
2022-10-25 17:20:33 -0400

    Create class for simulating random datasets

--- outcome_adaptive_lasso/simulation.py
+++ outcome_adaptive_lasso/simulation.py
@@ -6,11 +6,11 @@ from math import log
 
 import numpy as np
 import pandas as pd
-from causallib.estimation import IPW
-from sklearn.linear_model import LogisticRegression, LinearRegression
+
+from utils import get_psd_matrix
 
 
-class Simulate:
+class SimulateDataset:
     def __init__(self,
                  num_c,
                  num_p,
@@ -18,7 +18,8 @@ class Simulate:
                  num_covariates,
                  coef_c,
                  coef_p,
-                 coef_i):
+                 coef_i,
+                 eta):
         self.num_c = num_c
         self.num_p = num_p
         self.num_i = num_i
@@ -32,7 +33,7 @@ class Simulate:
             self.coef_c = [coef_c, coef_c]
         self.coef_p = coef_p
         self.coef_i = coef_i
-
+        self.eta = eta
         if self.num_s < 0:
             raise SyntaxError('the number of spurious variables < 0')
 
@@ -54,10 +55,10 @@ class Simulate:
     def load_scenario(self):
         """Utility function to load predefined scenarios"""
         c_indexes = list(range(self.num_c))
-        p_indexes = [i for i in range(start=self.num_c,
-                                      stop=self.num_p+self.num_c)]
-        i_indexes = [i for i in range(start=self.num_p,
-                                      stop=self.num_p+self.num_c+self.num_i)]
+        p_indexes = list(range(self.num_c,
+                               self.num_p+self.num_c))
+        i_indexes = list(range(self.num_p,
+                               self.num_p+self.num_c+self.num_i))
         nu = np.zeros(self.num_covariates)
         beta = np.zeros(self.num_covariates)
         beta[c_indexes] = self.coef_c[0]
@@ -68,33 +69,22 @@ class Simulate:
 
     def generate_dataset(self):
         # covariance matrix of the Gaussian covariates.
-        cov_x = np.eye(self.num_covariates) +\
-                ~np.eye(self.num_covariates, dtype=bool) * rho
-        cov_x = np.random.rand(self.num_covariates, self.num_covariates)
+        cov_x = get_psd_matrix(self.num_covariates, diagonal=1)
 
-
-        # Variance of each covariate is 1,
-        # correlation coefficient of every pair is rho
-        X = np.random.multivariate_normal(mean=0 * np.ones(self.num_covariates), cov=cov_x,
-                                          size=n)  # shape (n,self.num_covariates)
+        X = np.random.multivariate_normal(mean=0 * np.ones(self.num_covariates),
+                                          cov=cov_x,
+                                          size=self.num_covariates)
         # Normalize covariates to have 0 mean unit std
         scaler = StandardScaler(copy=False)
         scaler.fit_transform(X)
 
         # Load beta and nu from the predefined scenarios
-        beta, nu = load_scenario(scenario_num, self.num_covariates)
-        A = np.random.binomial(np.ones(n, dtype=int), expit(np.dot(X, nu)))
-        Y = np.random.randn(n) + eta * A + np.dot(X, beta)
-        col_names = generate_col_names(self.num_covariates)
+        beta, nu = self.load_scenario()
+        A = np.random.binomial(np.ones(self.num_covariates, dtype=int),
+                               expit(np.dot(X, nu)))
+        Y = np.random.randn(self.num_covariates) + self.eta*A + np.dot(X, beta)
+        col_names = self.generate_col_names()
         df = pd.DataFrame(np.hstack([A.reshape(-1, 1), Y.reshape(-1, 1), X]),
                           columns=col_names)
         return df
 
-
-    def calc_ate_ipw(A, Y, X):
-        ipw = IPW(LogisticRegression(solver='liblinear', penalty='l1', C=1e2,
-                               max_iter=500), use_stabilized=False).fit(X, A)
-        weights = ipw.compute_weights(X, A)
-        outcomes = ipw.estimate_population_outcome(X, A, Y, w=weights)
-        effect = ipw.estimate_effect(outcomes[1], outcomes[0])
-        return effect[0]
